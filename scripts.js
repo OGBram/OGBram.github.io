966e707910e5c0385d8a0c7ac6bcc3cf42d1f7ef
OGBram <156800524+OGBram@users.noreply.github.com>
2024-01-30 07:05:33 -0700

    v1
    v1

---
+++ scripts.js
@@ -0,0 +1,28 @@
+const toggleBtn = document.getElementById("toggleBtn");
+const mainNav = document.getElementById("mainNav");
+
+toggleBtn.addEventListener("mouseover", () => {
+    mainNav.style.display = "block";
+ });
+
+toggleBtn.addEventListener("mouseout", () => {
+    setTimeout(() => {
+     mainNav.style.display = "none";
+   }, 2000); // Adjust the delay in milliseconds (e.g., 500ms for half a second)
+ }) 
+ 
+function openAboutModal() {
+    document.getElementById('aboutModal').style.display = 'block';
+}
+
+function closeAboutModal() {
+    document.getElementById('aboutModal').style.display = 'none';
+}
+
+function openContactsModal() {
+    document.getElementById('contactsModal').style.display = 'block';
+}
+
+function closeContactsModal() {
+    document.getElementById('contactsModal').style.display = 'none';
+}
