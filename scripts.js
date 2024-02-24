457929c60bb43d4995e6b1cfb9aa43172345a33e
OGBram <156800524+OGBram@users.noreply.github.com>
2024-02-24 12:40:52 -0700

    11

--- scripts.js
+++ scripts.js
@@ -42,11 +42,10 @@ window.addEventListener('load', function(){
     class Cats {
             constructor(game) {
                 this.game = game;
-                this.dx = 0;
-                this.dy = 0;
-                this.speedModifier = 1;
+                this.catspeed = 1;
                 this.spriteWidth = 32;
                 this.spriteHeight = 32;
+                this.free = true;
                 this.width = this.spriteWidth;
                 this.height = this.spriteHeight;
                 this.x = this.game.width * 0.5 - this.width * 0.5;
@@ -55,32 +54,48 @@ window.addEventListener('load', function(){
                 this.frameX = 0;
                 this.frameY = 1;
                 this.maxFrame = 8;
+                this.start();
             }
-            update(){
-                
-                this.y = this.y - 1;
-                
-            }
+
             draw(context,) {
-                this.frameX <= this.maxFrame ? this.frameX++ : this.frameX = 0;
-    
+                // this.frameX <= this.maxFrame ? this.frameX++ : this.frameX = 0;
+                if (!this.free){
                 context.drawImage(
                     this.image,
                     this.frameX * this.spriteWidth,
                     this.frameY * this.spriteHeight,
                     this.spriteWidth,
                     this.spriteHeight,
-                    this.x+90,
-                    this.y+30,
-                    this.width/2,
-                    this.height/2,
-                );
-                
+                    this.x,
+                    this.y,
+                    this.width,
+                    this.height,
+                );}
             }
+            update(){
+                if (!this.free){
+                    this.frameX += this.catspeed;
+                    if(this.frameX >= this.maxFrame){
+                     this.reset(); 
+                    }
+                }
+            }        
+            reset(){
 
-        }        
-        class Background {
-            constructor(game) {
+                this.frameX = 0;
+            
+            }
+            start() {
+                if (this.free) {
+                    this.x = Math.random() * this.game.width;
+                    this.y = Math.random() * this.game.height;
+                    this.free = false;
+                }           
+            }    
+    }
+            
+    class Background {
+        constructor(game) {
                 this.game = game;
                 this.dx = 0;
                 this.dy = 0;
@@ -156,66 +171,91 @@ window.addEventListener('load', function(){
             this.width = this.canvas.width;
             this.height = this.canvas.height;
             this.player = new Player(this);
-            this.cat = new Cats(this);
             this.background = new Background(this);
             this.background2 = new Background2(this);
-            this.mouse = {
-                x: this.width * 0.5,
-                y: this.height * 0.5,
-                pressed: false
+            this.catPool = [];
+            this.max = 15;
+            this.createCatPool();
+            this.getCat();
+            
+            // this.mouse = {
+            //     x: this.width * 0.5,
+            //     y: this.height * 0.5,
+            //     pressed: false            
+            // }
+
+            // window.addEventListener('mousedown', e => {
+            //     this.mouse.x = e.offsetX;
+            //     this.mouse.y = e.offsetY;
+            //     this.mouse.pressed = true;
+            // });
+
+            // window.addEventListener('mouseup', e => {
+            //     this.mouse.x = e.offsetX;
+            //     this.mouse.y = e.offsetY;
+            //     this.mouse.pressed = true;
+            // });
+
+            // window.addEventListener('mousemove', e => {
+            //     if (this.mouse.pressed) {
+            //         this.mouse.x = e.offsetX;
+            //         this.mouse.y = e.offsetY;
+            //     }
+            // });
+        }
+        createCatPool(){
+            for (let i = 0; i < this.max; i++){
+                this.catPool.push(new Cats(this));
             }
+        }   
 
-            window.addEventListener('mousedown', e => {
-                this.mouse.x = e.offsetX;
-                this.mouse.y = e.offsetY;
-                this.mouse.pressed = true;
-            });
-
-            window.addEventListener('mouseup', e => {
-                this.mouse.x = e.offsetX;
-                this.mouse.y = e.offsetY;
-                this.mouse.pressed = true;
-            });
-
-            window.addEventListener('mousemove', e => {
-                if (this.mouse.pressed) {
-                    this.mouse.x = e.offsetX;
-                    this.mouse.y = e.offsetY;
+        getCat() {
+            for (let i = 0; i < this.catPool.length; i++) {
+                if (this.catPool[i].free) {
+                    return this.catPool[i];
                 }
-            });
+            }
+            return null; 
         }
 
         render(context) {
-            this.player.draw(context);
-
+            this.catPool.forEach(cat => {
+                if (!cat.free) {
+                    cat.update(); 
+                    cat.draw(context);
+                }
+            });
+        
             this.background.draw(context);
             this.background2.draw(context, 0.5);
-            this.cat.update(context);
-            this.cat.draw(context);
         }
 
     }
+
     const game = new Game(canvas);
-    
+
     var lastTime;
-    var requiredElapsed = 1000 / 6; // desired interval is 10fps
+    var requiredElapsed = 1000 / 6; 
     
     requestAnimationFrame(loop);
     
     function loop(now) {
         requestAnimationFrame(loop);
-        
+    
         if (!lastTime) { lastTime = now; }
         var elapsed = now - lastTime;
     
         if (elapsed > requiredElapsed) {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
-
+    
             game.render(ctx);
+            game.catPool.forEach(cat => {
+                cat.update();
+            });
+    
             lastTime = now;
         }
-        
-    }
+    } 
 });
 
 // document.getElementById("toggleNav").addEventListener("click", function() {
