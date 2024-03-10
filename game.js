9116b6031f6cc6ef4528b87f641e3d22256fe72e
OGBram <156800524+OGBram@users.noreply.github.com>
2024-03-10 09:35:33 -0700

    111

--- game.js
+++ game.js
@@ -225,7 +225,42 @@ window.addEventListener('load', function(){
                     this.y = 0;
                 }
         }    
-
+        class FireSheet {
+            constructor(game) {
+                    this.game = game;
+                    this.dx = 0;
+                    this.dy = 0;
+                    this.speedModifier = 1;
+                    this.spriteWidth = 53;
+                    this.spriteHeight = 152;
+                    this.width = this.spriteWidth;
+                    this.height = this.spriteHeight;
+                    this.x = this.game.width * 0.5 - this.width * 0.5;
+                    this.y = this.game.height * 0.5 - this.height * 0.5;
+                    this.image = document.getElementById("fire");
+                    this.frameX = 0;
+                    this.frameY = 0;
+                    this.maxFrame = 6;
+                }
+        
+                draw(context,) {
+                    this.frameX <= this.maxFrame ? this.frameX++ : this.frameX = 0;
+                    context.save();
+                    ctx.globalAlpha = 0.5;
+                    context.drawImage(
+                        this.image,
+                        this.frameX * this.spriteWidth,
+                        this.frameY * this.spriteHeight,
+                        this.spriteWidth,
+                        this.spriteHeight,
+                        this.x+53,
+                        this.y+23,
+                        this.width/3,
+                        this.height/3,
+                    );
+                    context.restore();    
+                }
+            }
     class Ui {
         constructor(game) {
                 this.game = game;
@@ -276,6 +311,7 @@ window.addEventListener('load', function(){
             this.starFeild = new Starfeild(this);
             this.stage = new Stage(this);
             this.background = new Background(this);
+            this.fireSheet = new FireSheet(this);
             // this.Ui = new Ui(this);
             this.catPool = [];
             this.max = 1;
@@ -345,6 +381,7 @@ window.addEventListener('load', function(){
             });
             this.mouse.pressed = false;
             this.background.draw(context);
+            this.fireSheet.draw(context);
 
             ctx.fillText("\"Birds migrating,", 205, 354);
             ctx.fillText("Ah—where they are headed,", 207, 376);
