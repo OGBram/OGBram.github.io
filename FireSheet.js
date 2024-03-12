52ed4ff85fd936d7eb05cb908773f944c6fe1477
OGBram <156800524+OGBram@users.noreply.github.com>
2024-03-12 05:58:52 -0700

    file system update

--- FireSheet.js
+++ FireSheet.js
@@ -1,37 +1,37 @@
-class Background {
+class FireSheet {
     constructor(game) {
             this.game = game;
             this.dx = 0;
             this.dy = 0;
             this.speedModifier = 1;
-            this.spriteWidth = 149;
-            this.spriteHeight = 103;
+            this.spriteWidth = 53;
+            this.spriteHeight = 152;
             this.width = this.spriteWidth;
             this.height = this.spriteHeight;
             this.x = this.game.width * 0.5 - this.width * 0.5;
             this.y = this.game.height * 0.5 - this.height * 0.5;
-            this.image = document.getElementById("waterA");
+            this.image = document.getElementById("fire");
             this.frameX = 0;
             this.frameY = 0;
-            this.maxFrame = 3;
+            this.maxFrame = 6;
         }
 
         draw(context,) {
             this.frameX <= this.maxFrame ? this.frameX++ : this.frameX = 0;
             context.save();
-            ctx.globalAlpha = 0.4;
+            ctx.globalAlpha = 0.5;
             context.drawImage(
                 this.image,
                 this.frameX * this.spriteWidth,
                 this.frameY * this.spriteHeight,
                 this.spriteWidth,
                 this.spriteHeight,
-                this.x-124,
-                this.y+46,
-                this.width,
-                this.height,
+                this.x+53,
+                this.y+23,
+                this.width/3,
+                this.height/3,
             );
             context.restore();    
         }
     }
-export default Background;
+export default FireSheet;
---
+++ Heart.js
@@ -0,0 +1,50 @@
+class Heart {
+    constructor(game) {
+            this.game = game;
+            this.dx = 0;
+            this.dy = 0;
+            this.speedModifier = 1;
+            this.spriteWidth = 16;
+            this.spriteHeight = 16;
+            this.width = this.spriteWidth;
+            this.height = this.spriteHeight;
+            this.x = 50 + Math.random()*100;
+            this.y = 75 + Math.random()*100;
+            this.image = document.getElementById("hearts");
+            this.frameX = 0;
+            this.frameY = Math.floor(Math.random()*3);
+            this.maxFrame = 0;
+            this.speedX = 0;
+            this.speedY = 0;
+
+    }
+        draw(context,) {
+
+            context.save();
+            ctx.globalAlpha = 1.0;
+            context.drawImage(
+                this.image,
+                this.frameX * this.spriteWidth,
+                this.frameY * this.spriteHeight,
+                this.spriteWidth,
+                this.spriteHeight,
+                this.x,
+                this.y,
+                this.width/1.8,
+                this.height/1.8,
+            );
+            context.restore();    
+        }
+        update(){
+            this.speedX = Math.floor(Math.random()*25);
+            this.speedY = -3;
+            this.x += this.speedX;
+            this.y += this.speedY;
+
+        }
+        reset(){
+            this.x = 0;
+            this.y = 0;
+        }
+    }
+export default Heart;
